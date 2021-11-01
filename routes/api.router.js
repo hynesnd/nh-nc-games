6ef5247d3b74636e28ef7614d948106a370102a6
hynesnd <hynesnd@gmail.com>
2021-11-01 13:02:20 +0000

    GET categories: sets up categories router, controller and model

--- routes/api.router.js
+++ routes/api.router.js
@@ -1,3 +1,7 @@
+const app = require("../app");
 const apiRouter = require("express").Router();
+const categoriesRouter = require("./categories.router");
+
+app.use("/api/categories", categoriesRouter);
 
 module.exports = apiRouter;
---
+++ routes/categories.router.js
@@ -0,0 +1,4 @@
+const categoriesRouter = require("express").Router();
+const {} = require("../controllers/categories.controller");
+
+module.exports = categoriesRouter;
