69d9ab0645aed78e49e812340e2df48743589dd4
hynesnd <hynesnd@gmail.com>
2021-11-01 14:59:34 +0000

    completes GET api/categories

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -2,6 +2,9 @@ const { selectCategories } = require("../models/categories.model");
 
 exports.getCategories = (req, res, next) => {
   selectCategories()
-    .then(() => {})
+    .then((categories) => {
+      console.log(categories);
+      res.status(200).send({ categories });
+    })
     .catch(next);
 };
