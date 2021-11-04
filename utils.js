419ee4d9c06f4bc50515eea3a05441b55c33b04e
hynesnd <hynesnd@gmail.com>
2021-11-04 15:06:11 +0000

    removes convertBigintStrToNum function from utils, as it was made redundant by adding CASTS to review SQL queries

--- utils.js
+++ utils.js
@@ -1,15 +1,6 @@
 const format = require("pg-format");
 const db = require("./db/connection");
 
-//psql bigInt datatype is coerced to string on output.
-//for consistency of testing I decided to write this util to drop into models
-//and coerce the strings to Number as I am confident that numbers in this app will not exceed 53 bits..
-exports.convertBigintStrToNum = (str) => {
-  if (!str || str.length === 0) return null;
-
-  return Number(str);
-};
-
 exports.checkExists = async (table, column, value) => {
   const queryString = format(
     `
