53afb939312773145ac17a397f703503082ea3e5
hynesnd <hynesnd@gmail.com>
2021-11-04 15:27:12 +0000

    completes Status 200: GET /api/users

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,6 +2,8 @@ const { selectAllUsers } = require("../models/users.model");
 
 exports.getAllUsers = (req, res, next) => {
   selectAllUsers()
-    .then(() => {})
+    .then((users) => {
+      res.status(200).send({ users });
+    })
     .catch(next);
 };
--- models/users.model.js
+++ models/users.model.js
@@ -1,3 +1,12 @@
 const db = require("../db/connection");
 
-exports.selectAllUsers = async () => {};
+exports.selectAllUsers = async () => {
+  const selectStr = `
+SELECT 
+username
+FROM users;`;
+
+  const { rows } = await db.query(selectStr);
+
+  return rows;
+};
