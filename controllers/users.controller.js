2b9631223cfa7767bdaa6ee1689efaf65d3680f1
hynesnd <hynesnd@gmail.com>
2021-11-04 15:37:36 +0000

    adds getUser skeleton function to users controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,15 @@
-const { selectAllUsers } = require("../models/users.model");
+const { selectAllUsers, selectUser } = require("../models/users.model");
 
 exports.getAllUsers = (req, res, next) => {
   selectAllUsers()
     .then((users) => {
       res.status(200).send({ users });
     })
+    .catch(next);
+};
+
+exports.getUser = (req, res, next) => {
+  selectUser()
+    .then(() => {})
     .catch(next);
 };
