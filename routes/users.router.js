f2777b91e1334d633160f3547aa0c0f59634dec9
hynesnd <hynesnd@gmail.com>
2021-11-04 15:38:15 +0000

    adds users/:username routing and get method to users router

--- routes/users.router.js
+++ routes/users.router.js
@@ -1,7 +1,8 @@
-const { getAllUsers } = require("../controllers/users.controller");
+const { getAllUsers, getUser } = require("../controllers/users.controller");
 
 const usersRouter = require("express").Router();
 
 usersRouter.route("/").get(getAllUsers);
+usersRouter.route("/:username").get(getUser);
 
 module.exports = usersRouter;
