b629eb0f25ea753020188d165a50b4c51f1a7dc7
hynesnd <hynesnd@gmail.com>
2021-11-04 15:23:47 +0000

    adds userRouter to api router

--- routes/api.router.js
+++ routes/api.router.js
@@ -3,11 +3,13 @@ const apiRouter = require("express").Router();
 const categoriesRouter = require("./categories.router");
 const commentsRouter = require("./comments.router");
 const reviewsRouter = require("./reviews.router");
+const usersRouter = require("./users.router");
 const { getEndpoints } = require("../controllers/api.controller");
 
 apiRouter.get("/", getEndpoints);
 apiRouter.use("/categories", categoriesRouter);
 apiRouter.use("/reviews", reviewsRouter);
 apiRouter.use("/comments", commentsRouter);
+apiRouter.use("/users", usersRouter);
 
 module.exports = apiRouter;
