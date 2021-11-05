a91c58d31c3b8a0afe4f897b35754bf12f25d832
hynesnd <hynesnd@gmail.com>
2021-11-05 11:52:52 +0000

    amends categories select to use explicit column names instead of *

--- models/categories.model.js
+++ models/categories.model.js
@@ -1,6 +1,6 @@
 const db = require("../db/connection");
 
 exports.selectCategories = async () => {
-  const { rows } = await db.query("SELECT * FROM categories;");
+  const { rows } = await db.query("SELECT slug, description FROM categories;");
   return rows;
 };
