246d16c87a06657eb0c60d2a2fd67d037ef79eff
hynesnd <hynesnd@gmail.com>
2021-11-05 10:23:29 +0000

    adds patch method to comments/:comment_id route in comments router

--- routes/comments.router.js
+++ routes/comments.router.js
@@ -1,7 +1,13 @@
-const { deleteComment } = require("../controllers/comments.controller");
+const {
+  deleteComment,
+  patchCommentVotes,
+} = require("../controllers/comments.controller");
 
 const commentsRouter = require("express").Router();
 
-commentsRouter.route("/:comment_id").delete(deleteComment);
+commentsRouter
+  .route("/:comment_id")
+  .delete(deleteComment)
+  .patch(patchCommentVotes);
 
 module.exports = commentsRouter;
