6fdb66a939081e2453d2eb717950ad3773172fac
hynesnd <hynesnd@gmail.com>
2021-11-22 16:40:52 +0000

    allows cross origin resource sharing

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const apiRouter = require("./routes/api.router");
 const {
@@ -7,6 +8,7 @@ const {
   handlePsqlErrors,
 } = require("./controllers/errors.controller");
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
